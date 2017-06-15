2647986bbef21f5a694bfda7ada3b19c82a4ddf8
ricktjwong <rickwtj@gmail.com>
2017-06-15 14:10:27 +0100

    sweep in actual

--- Particle_Tracing/Particle_Tracing_2.py
+++ Particle_Tracing/Particle_Tracing_2.py
@@ -17,27 +17,31 @@ plt.close('all')
 #%% Load Data
 
 # Load grid spacing
-x,y,z = vtk_subs.import_space('SW_2\\x13_rho1-2000.vti')
+x,y,z = vtk_subs.import_space('source/x13_rho1-2000.vti')
 dx = x[1]-x[0]
 dy = y[1]-y[0]
 dz = z[1]-z[0]
 
 # Load variables
-rho = vtk_subs.import_scalar('SW_2\\x13_rho1-2000.vti','rho1')
-E = vtk_subs.import_vector('SW_2\\x13_Evec-2000.vti','Evec')
-B = vtk_subs.import_vector('SW_2\\x13_Bvec-2000.vti','Bvec')
+rho = vtk_subs.import_scalar('source/x13_rho1-2000.vti','rho1')
+E = vtk_subs.import_vector('source/x13_Evec-2000.vti','Evec')
+B = vtk_subs.import_vector('source/x13_Bvec-2000.vti','Bvec')
+
+print np.shape(B)
+print np.shape(E)
+print np.shape(rho)
 
 r_IB = 6*dx # Inner boundary
-
-# Plot
-
-iy = int(0.5*y.shape[0])
-
-fig, ax = plt.subplots(figsize=(11,5))
-p = ax.pcolormesh(x/RP,z/RP,rho[:,iy,:].T)
-plt.colorbar(p,ax=ax)
-ax.set(aspect='equal',xlabel='x')
-plt.show()
+#
+## Plot
+#
+#iy = int(0.5*y.shape[0])
+#
+#fig, ax = plt.subplots(figsize=(11,5))
+#p = ax.pcolormesh(x/RP,z/RP,rho[:,iy,:].T)
+#plt.colorbar(p,ax=ax)
+#ax.set(aspect='equal',xlabel='x')
+#plt.show()
 
 #%% Particle Streamtracing
 
@@ -113,6 +117,8 @@ q = qe
 
 eta0 = np.array([-18*RP,-5*RP,2*RP,4e3,0,0])
 
+#eta0 = np.array([-17*RP, 0.0, 0.5*RP, 100e3, 0.0, 0.0])
+
 t = np.linspace(0,1,1000)*600
 eta = odeint(lorentz,eta0,t,args=(m,q,x,y,z,E,B,d))
 
@@ -147,5 +153,62 @@ ax.set(xlim=[x.min()/RP,x.max()/RP],ylim=[y.min()/RP,y.max()/RP],zlim=[z.min()/R
       aspect='equal')
 plt.show()
 
+def CheckEnter(trajectory):
+    Enter = False
+    last_posvel = trajectory[-1]
+    xx, xy, xz = last_posvel[0], last_posvel[1], last_posvel[2]
+    r_ = np.array([xx,xy,xz])
+    r_mod = np.linalg.norm(r_)
+    if r_mod <= RP:
+        Enter = True
+    elif r_mod > RP:
+        Enter = False
+    return Enter
+    
+def search(pos_vel, step, count, sweep_param):
+    soln_set = []
+    nonsoln_set = []
+    delta = step  # increment of initial x to sweep
+    n = 1           # counter
+    while n <= count:
+        traj = odeint(lorentz,eta0,t,args=(m,q,x,y,z,E,B,d))
+        if CheckEnter(traj) == True:
+            soln_set.append(pos_vel)
+            print traj[-1]
+            print "Enter!"
+        elif CheckEnter(traj) == False:
+            mod_r=[]
+            posx,posy,posz = traj[:,0], traj[:,1], traj[:,2]
+            posr = np.vstack((posx,posy,posz)).T
+            for i in posr:
+                mod_r.append(np.linalg.norm(i))
+            print np.min(mod_r)
+            #print traj[-1]
+            print "Not Enter!"
+            nonsoln_set.append(pos_vel)
+        
+        if sweep_param == "posx":
+            pos_vel = pos_vel + np.array([delta,0.0,0.0,0.0,0.0,0.0])
+        elif sweep_param == "posy":
+            pos_vel = pos_vel + np.array([0.0,delta,0.0,0.0,0.0,0.0])
+        elif sweep_param == "posz":
+            pos_vel = pos_vel + np.array([0.0,0.0,delta,0.0,0.0,0.0])
+        elif sweep_param == "velx":
+            pos_vel = pos_vel + np.array([0.0,0.0,0.0,delta,0.0,0.0])
+        n += 1
+    
+    return (soln_set, nonsoln_set)
 
+#soln_pos, nonsoln_pos = search(xinit, RE, 10, 'pos')
+soln_vel, nonsoln_vel = search(eta0, RP, 10, 'posx')
+
+def saveToFile(valuesToWrite, sweep_param):
+    text_file = open('Data/'+sweep_param+".txt", "w")
+    headers=['Entered','Not entered']
+    for i,j in zip(headers,valuesToWrite):
+        text_file.write(str(i)+": %s \n\n" % str(j))
+    text_file.close()
+
+#saveToFile([soln_pos, nonsoln_pos], 'posx')
+saveToFile([soln_vel, nonsoln_vel], 'velx')
 
