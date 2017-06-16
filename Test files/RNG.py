1cb7ef5dd572b70c8599e93388913ae55ad9cc14
DESKTOP-9F4QPDP\Danielsrq <danielsrq2015@gmail.com>
2017-06-16 12:03:03 +0100

    posvel generator.

---
+++ Test files/RNG.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 10:11:39 2017
+
+@author: Danielsrq
+"""
+
+import numpy as np
+
+## where lower_r upper_r are range of coordinates ExpectV is expectation of final V and n is size
+def rng_posvel(lower_x , upper_x , lower_y , upper_y , lower_z , upper_z, ExpectV , n):
+    v_sq = ExpectV**2
+    Pos_Vel = []
+    v_ = []
+    
+    x_pos = np.random.randint(lower_x, upper_x,size = (n,1))
+    y_pos = np.random.randint(lower_y,upper_y,size=(n,1))
+    z_pos = np.random.randint(lower_z,upper_z,size=(n,1))
+    xyz_pos = np.hstack((x_pos,y_pos,z_pos))
+    print xyz_pos
+    
+    xyz_vel = (v_sq)*np.random.dirichlet(np.ones(3),size = n)
+    for i in xyz_vel:
+        vx,vy,vz = np.sqrt(i[0]), np.sqrt(i[1]), np.sqrt(i[2])
+        v_.append( [vx, vy, vz] )
+    print v_
+    
+    Pos_Vel = np.hstack((xyz_pos, v_))
+    
+    return Pos_Vel
+    
+rng1 = rng_posvel(-20, -10, -10, 10, -10, 10, 5, 3)        
